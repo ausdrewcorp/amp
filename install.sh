3463994dfdd1324c18f91c2f88cbc086b39572db
Andrew S <6315839+ausdrew@users.noreply.github.com>
2024-06-08 02:47:30 +0000

    wa

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create a symlink for server files so they're accessible from the file manager
+if [ ! -L "/AMP/killinuxfloor/server" ]; then
+    ln -s /home/amp /AMP/killinuxfloor/server
+fi
+
+cp -aP /home/steam/* /home/amp/
+chown -R amp:amp /home/amp 
+
+cd /home/amp && \
+find . -type l -exec bash -c 'ln -sfn "/home/amp$(readlink {} | cut -c12-)" {}' \; && \
+chown -R amp:amp /home/amp 
+ls -la /home/amp
