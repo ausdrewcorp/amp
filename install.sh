1da27100a46b49a66f7f5a97520f3b19d1755c56
Andrew S <6315839+ausdrew@users.noreply.github.com>
2024-06-08 05:19:35 +0000

    Refactor, going to try and do it properly I guess

--- install.sh
+++ install.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-
-# Create a symlink for server files so they're accessible from the file manager
-if [ ! -L "/AMP/killinuxfloor/server" ]; then
-    ln -s /home/amp /AMP/killinuxfloor/server
-fi
--- update.sh
+++ update.sh
@@ -1,7 +1,1 @@
 #!/bin/bash
-export TERM=xterm
-
-echo "Updating KF2 - may take a while, and the console only updates periodically. Please be patient."
-cd /home/amp
-./bin/killinuxfloor update
-
